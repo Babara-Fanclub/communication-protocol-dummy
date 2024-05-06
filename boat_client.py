4d67870c3886111918188285dceef50d6172fe0f
Chin <ngchinyuen02@gmail.com>
2024-05-06 17:34:30 +0800

    feat: Integrated with RPI

--- boat_client.py
+++ boat_client.py
@@ -3,7 +3,6 @@
 
 import logging
 import sys
-import random
 import time
 
 import serial
@@ -15,6 +14,7 @@ from google.protobuf.message import Message, DecodeError
 from comm_proto import connection_pb2, data_pb2, latlng_pb2, timestamp_pb2
 
 PacketType = connection_pb2.Packet.PacketType
+nav_data = []
 
 
 def send_data(port: serial.Serial, packet_type: PacketType, message: Message):
@@ -40,64 +40,77 @@ def handle_data(port: serial.Serial, data: bytes):
         return
 
     main_packet = packet.data
-    match packet.type:
-        case PacketType.CONNECT:
-            try:
-                connection_pb2.Connect().ParseFromString(main_packet)
-                logging.info("Recevied connection request, replying...")
-                send_data(
-                    port, PacketType.CONNECT, connection_pb2.Connect(version="0.1.0")
-                )
-            except DecodeError:
-                logging.error("Expected a Connect message but received and invalid one")
-                return
-        case PacketType.RECEIVED:
-            try:
-                connection_pb2.Received().ParseFromString(main_packet)
-            except DecodeError:
-                logging.error(
-                    "Expected a Received message but received and invalid one"
-                )
-                return
-        case PacketType.PATH_DATA:
-            try:
-                path_data = data_pb2.PathData()
-                path_data.ParseFromString(main_packet)
-                logging.debug("PathData: %s", path_data)
-                send_data(
-                    port, PacketType.RECEIVED, connection_pb2.Received(version="0.1.0")
-                )
-                logging.info("Received a new path! Sending to path planner.")
-                # TODO: Send the information to the controller
-            except DecodeError:
-                logging.error(
-                    "Expected a PathData message but received and invalid one"
-                )
-                return
-        case _:
-            logging.error("Received Unexpected Packet Type: %s", packet.type)
+    if packet.type == PacketType.CONNECT:
+        try:
+            connection_pb2.Connect().ParseFromString(main_packet)
+            logging.info("Recevied connection request, replying...")
+            send_data(
+                port, PacketType.CONNECT, connection_pb2.Connect(version="0.1.0")
+            )
+        except DecodeError:
+            logging.error("Expected a Connect message but received and invalid one")
             return
+    elif packet.type == PacketType.RECEIVED:
+        try:
+            connection_pb2.Received().ParseFromString(main_packet)
+        except DecodeError:
+            logging.error(
+                "Expected a Received message but received and invalid one"
+            )
+            return
+    elif packet.type == PacketType.PATH_DATA:
+        try:
+            path_data = data_pb2.PathData()
+            path_data.ParseFromString(main_packet)
+            logging.debug("PathData: %s", path_data)
+            send_data(
+                port, PacketType.RECEIVED, connection_pb2.Received(version="0.1.0")
+            )
+            logging.info("Received a new path! Sending to path planner.")
+            points = [[point.latitude, point.longitude] for point in path_data.points]
+            global nav_data
+            # nav_data = nav.nav(points)
+        except DecodeError:
+            logging.error(
+                "Expected a PathData message but received and invalid one"
+            )
+            return
+    else:
+        logging.error("Received Unexpected Packet Type: %s", packet.type)
+        return
 
     return packet.type
 
 
 def send_boat_data(port: serial.Serial):
     """Sends a fake data to the desktop."""
-    center_point = (101.87463, 2.94375)
+    features = []
+    for i, data in enumerate(nav_data):
+        time_stamp = timestamp_pb2.Timestamp(seconds=int(data[3]))
+        point = latlng_pb2.LatLng(
+            latitude=data[4],
+            longitude=data[5],
+        )
+        layer = data[2]
+        if layer == 1:
+            layer_name = data_pb2.BoatData.Layer.SURFACE
+        elif layer == 2:
+            layer_name = data_pb2.BoatData.Layer.MIDDLE
+        elif layer == 3:
+            layer_name = data_pb2.BoatData.Layer.SEA_BED
+        else:
+            layer_name = data_pb2.BoatData.Layer.SEA_BED
+            
+        feature = data_pb2.BoatData.BoatDataFeature(
+            temperature=data[0],
+            depth=data[1],
+            layer=layer_name,
+            time=time_stamp,
+            geometry=point,
+        )
+        features.append(feature)
 
-    time_stamp = timestamp_pb2.Timestamp(seconds=int(time.time()))
-    point = latlng_pb2.LatLng(
-        latitude=center_point[1] + random.uniform(-0.0001, 0.0001),
-        longitude=center_point[0] + random.uniform(-0.0001, 0.0001),
-    )
-    feature = data_pb2.BoatData.BoatDataFeature(
-        temperature=random.uniform(35, 35),
-        depth=random.uniform(0, 3),
-        layer=data_pb2.BoatData.Layer.SEA_BED,
-        time=time_stamp,
-        geometry=point,
-    )
-    data = data_pb2.BoatData(version="0.1.0", features=(feature,))
+    data = data_pb2.BoatData(version="0.1.0", features=features)
     logging.debug("Sent Data: %s", data)
     send_data(port, PacketType.BOAT_DATA, data)
 
